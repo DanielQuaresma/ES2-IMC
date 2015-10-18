38c0b5e519e9042312b7bc2834b9b8fc36e3a1ef
DanielQuaresma <cipriano.96@hotmail.com>
2015-10-18 18:33:42 -0200

    Finalizado

--- IMC/src/projeto/es2/imc/IMC.java
+++ IMC/src/projeto/es2/imc/IMC.java
@@ -20,30 +20,50 @@ public class IMC {
     public String calculaImc(double a, int p, String s) {
         String msg = "";
         s = s.toUpperCase();
-        
+
         if ((p <= 0) || (a < 0.60) || (a > 2.40) || (s.equals(""))) {
             msg = "Valores Inválidos";
             return msg;
         }
-        double c = p / (a*a);
-       
+        double c = p / (a * a);
+        
+        //IMC Baixo
+        if ((c >= 17.9) && (c <= 18.9) && (s.equals("M"))) {
+            return msg = "Você está abaixo do peso recomendado.";
+        }
+
+        if ((c >= 15) && (c <= 17.9) && (s.equals("F"))) {
+            return msg = "Você está abaixo do peso recomendado.";
+        }
+
+        //IMC Ideal
         if ((c >= 19) && (c <= 24.9) && (s.equals("M"))) {
-            return msg = "Você está muito bem! Continue assim!";            
+            return msg = "Você está muito bem! Continue assim!";
+        }
+
+        if ((c >= 18) && (c <= 24.4) && (s.equals("F"))) {
+            return msg = "Você está muito bem! Continue assim!";
         }
         
-        if((c >= 18) && (c <= 24.4) && (s.equals("F"))) {
-            return msg = "Você está muito bem! Continue assim!";            
-        } 
+        //IMC Moderado
+        if ((c >= 25) && (c <= 27.7) && (s.equals("M"))) {
+            return msg = "Você está acima do peso recomendado. Cuidado!";
+        }
         
-       if((c >= 17.9) && (c <= 18.9) && (s.equals("M"))) {
-            return msg = "Você está abaixo do peso recomendado.";            
-       } 
-
-       if((c >= 15) && (c <= 17.9) && (s.equals("F"))) {
-            return msg = "Você está abaixo do peso recomendado.";            
-       }
-            
-            
+        if ((c >= 24.5) && (c <= 27.2) && (s.equals("F"))) {
+            return msg = "Você está acima do peso recomendado. Cuidado!";
+        }
+        
+        // IMC Elevado
+        if ((c >=27.8) && (s.equals("M"))) {
+            return msg = "Você está Obeso. Procure o acompanhamento de um nutricionista e realizar mais atividades físicas!";
+        }
+        
+        if ((c >= 27.3) && (s.equals("F"))) {
+            return msg = "Você está Obeso. Procure o acompanhamento de um nutricionista e realizar mais atividades físicas!";
+        }
+        
+        
         return "";
     }
 
